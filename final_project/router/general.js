1342b0b957ab520bd151c2b4c54f0ba67a505f7e
Matias <matiasenriquegarciadev@gmail.com>
2023-09-09 00:49:02 -0300

    Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,9 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const formattedBookList = JSON.stringify(books, null, 2);
+  
+  return res.status(200).json({ books: formattedBookList });
 });
 
 // Get book details based on ISBN
