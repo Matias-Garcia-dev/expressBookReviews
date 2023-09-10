7c9ff826323e136d7c069c0ad946fbe16ac1d5b9
Matias <matiasenriquegarciadev@gmail.com>
2023-09-10 14:54:56 -0300

    iisbn search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,10 +18,14 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = parseInt(req.params.isbn);
+  if (books[isbn]) {
+    res.status(200).json(books[isbn]);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
